ec7f6c878f38f37ea404ba2d31f9d555a15892ce
Brookeinternet <brookieross46@outlook.com>
2025-07-18 09:53:05 -0400

    Update .eslintrc.js

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,10 +1,10 @@
-module.exports = {
+export default {
   env: {
     es6: true,
     node: true,
   },
   parserOptions: {
-    "ecmaVersion": 2018,
+    ecmaVersion: 2018,
   },
   extends: [
     "eslint:recommended",
@@ -13,7 +13,7 @@ module.exports = {
   rules: {
     "no-restricted-globals": ["error", "name", "length"],
     "prefer-arrow-callback": "error",
-    "quotes": ["error", "double", {"allowTemplateLiterals": true}],
+    "quotes": ["error", "double", { "allowTemplateLiterals": true }],
   },
   overrides: [
     {
